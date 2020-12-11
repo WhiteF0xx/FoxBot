88faafff2ab5a516e544c4d140ed3d75b7e8dea1
megamemes <75814328+megamemes@users.noreply.github.com>
2020-12-11 15:25:56 +0200

    Add files via upload

---
+++ commands/avatar.js
@@ -0,0 +1,30 @@
+module.exports = {
+    name:'avatar',
+    description: 'Gets the avatar of a user.',
+    alliases: ['profile','pic'],
+    args: true,
+    usage: '@[user] <size>',
+    example: '&avatar @FoxBot small',
+
+    execute(message,args,Discord,bot){
+
+        message.channel.send('hello there!')
+
+        ///Get users profile icon.
+        const user = message.mentions.users.first() //|| message.author;
+
+        if (args[1] === 'small') {
+            sz = 128
+        }else if (args[1] === 'medium') {
+            sz = 256
+        }else {
+            sz = 1024
+        }
+
+        let Embed = new Discord.MessageEmbed()
+            .setTitle(`**${user.username}**'s avatar`)
+            .setColor(0x82eb11)
+            .setImage(user.displayAvatarURL({size: +sz}))
+        message.channel.send(Embed);
+    }
+}
