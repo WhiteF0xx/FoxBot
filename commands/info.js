04f31b568d5a7049ab75dff90de1318e0b209325
megamemes <75814328+megamemes@users.noreply.github.com>
2020-12-20 00:37:13 +0200

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -1,23 +1,21 @@
-//const { prefix } = require('../config.json')
-//const { command } = require('../commands')
-
 module.exports = {
     name:`info`,
     description: `Info about the bot.`,
-    usage:'verison',
+    usage:['<verison>' , '<servers>'],
     cooldown: 5,
     args: true,
 
-    execute(message,args,){
+    execute(message,args,Discord,bot){
 
         let version = '1.4';
 
-        if (args[0] === 'version' ||  args[0] === 'ver'){
+        if (args[0] === 'version' || args[0] === 'ver'){
             message.channel.send('**Version** ' + version);
+        }else if(args[0] === 'servers'){
+            bot.guilds.cache.forEach((guild) => 
+            message.channel.send(`**${guild.name}** has a total of **${guild.memberCount}** members.`))
         }
         else {
-            //  message.channel.send(`Argument "${args[0]}" is wrong.
-            //  Try \`${prefix}${this.name} ${this.usage}\``)
             message.channel.send('**Invalid command.**')
         }
 
