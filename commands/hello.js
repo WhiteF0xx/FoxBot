d75bc341067a11ec23130054e86a7ce26ccba235
megamemes <75814328+megamemes@users.noreply.github.com>
2020-12-20 00:50:45 +0200

    Update hello.js

--- commands/hello.js
+++ commands/hello.js
@@ -1,7 +1,8 @@
 module.exports = {
     name:`hello`,
     description: `The bot says hello.`,
-    cooldown: 3,
+    aliases: ['hi'],
+    cooldown: 5,
     
     execute(message,args ,){
             message.channel.send(`**hewwo UwU**`)  
