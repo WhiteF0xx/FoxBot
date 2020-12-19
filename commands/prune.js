7b257a36c9583bd9825dec0989d800492166f1f7
megamemes <75814328+megamemes@users.noreply.github.com>
2020-12-20 00:42:07 +0200

    Update prune.js

--- commands/prune.js
+++ commands/prune.js
@@ -1,5 +1,3 @@
-
-
 module.exports = {
     name:`prune`,
     description: `Deletes messages.`,
@@ -15,8 +13,7 @@ module.exports = {
         if (isNaN(args[0])) return message.channel.send("Please input a valid number.").then(d => d.delete({timeout: 5000}));                // isNaN = is Not a Number. If the user doesnt input a number
         if (args[0] >100) return message.channel.send("I can't prune more than 100 messages.").then(d => d.delete({timeout: 5000}));         // If requested to delete more than 100 messages.
         if (args[0] < 1) return message.channel.send("Prune at least one message.").then(d => d.delete({timeout: 5000}));                    // If the user inputs 0 or lower. (The bot must delete more than one msg.)
-            message.delete();
-            message.channel.bulkDelete(args[0]);
+            message.channel.bulkDelete(args[0],true);
             message.channel.send(`Deleted **${args[0]}** messages!`).then(d => d.delete({timeout: 5000}))                                    // How long this message will be deleted in ms. 
             .then(messages => message.channel.send)
             .catch(() =>message.channel.send(`Something went wrong, while deleting messages.`))                                              // This error will displayd when the bot doesn't have an accsess to do it.                                       
